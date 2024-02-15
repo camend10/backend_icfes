12ef1aa954f99e8fdd39d19485586553b95d390f
Carlos Mendoza <carlosmario.mendoza@gmail.com>
2024-02-15 16:14:54 -0500

    FEAT: Proyecto Backend ICFES

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('identificacion', 20);
+            $table->string('name', 50);
+            $table->string('email')->unique();
+            $table->string('username', 20);
+            $table->string('password');
+            $table->string('telefono', 30);
+            $table->string('direccion', 255)->nullable();
+            $table->integer('curso')->unsigned();
+            $table->string('estado', 20);
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
